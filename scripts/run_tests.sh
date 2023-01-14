abc2f45b57632096393b8d9d9feda0acc0730218
Ivan Ilic <ivan.ilic@student.hpi.de>
2023-01-14 15:10:54 +0100

    Clean up scripts

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# ./build.sh
 python3 -u scripts/run_tests.py 4 build default | tee default_test_results_ac922.txt
 python3 -u scripts/run_tests.py 4 build skew | tee skew_test_results_ac922.txt
 python3 -u scripts/run_tests.py 4 build custom | tee custom_test_results_ac922.txt

becce1d612ba6f563f4442c1b628b55b5cf1890b
Ivan Ilic <ivan.ilic@student.hpi.de>
2023-01-14 17:24:16 +0100

    Fix slurm script

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Run this script from within the directory that you want to install rmg-sort into.
-pip install matplotlib
-pip install pandas
-git clone https://github.com/hpides/rmg-sort.git
-cd rmg-sort
